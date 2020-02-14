7562480fc79935d6f8c63ea00b735315e116b0ab
Urtii <yilmaz.onurcan@ieee.metu.edu.tr>
2020-02-14 03:02:14 +0300

    Servo code is now delay free.

--- orangutan_servo/src/orangutan_servo.ino
+++ orangutan_servo/src/orangutan_servo.ino
@@ -1,10 +1,11 @@
 /* RandoWalkerTall
-  by Amy R. Wu and Matthew A. Robertson
-  modified 20 May 2017
-  adapted to Particle Photon by Onurcan YILMAZ
+by Amy R. Wu and Matthew A. Robertson
+modified 20 May 2017
+adapted to Particle Photon by Onurcan YILMAZ
 */
 
 #include "MPU6050.h"
+#include "MQTT.h"
 
 Servo servoRL;  // create servo object to control a servo
 Servo servoRM;
@@ -58,6 +59,118 @@ int pushoff_LM = 0;
 int pushoffTimeSFM = 800; //550, 800, 650
 int pushoffTimeSFL = 800; //550, 800, 650, 450
 
+//Timers
+//laterals
+Timer lateral_contactDelay(999999,contactDelay_lateral,true);
+Timer lateral_pushOff(999999,pushoff_lateral,true);
+Timer lateral_swing(999999,swing_lateral,true);
+//medials
+Timer medial_contactDelay(999999,contactDelay_medial,true);
+Timer medial_pushOff(999999,pushoff_medial,true);
+Timer medial_swing(999999,swing_medial,true);
+
+
+//callback functions
+
+//laterals
+void contactDelay_lateral(){
+  time1 = millis();
+  //     Serial.println(time1-time2);
+
+  pushoff_RL = mid_RL - (mid_RL-down_RL)*(time1-time2)/pushoffTimeSFL; //450
+  if (pushoff_RL < down_RL) {
+    pushoff_RL = down_RL;
+  };
+
+  pushoff_LL = mid_LL + (down_LL-mid_LL)*(time1-time2)/pushoffTimeSFL;
+  if (pushoff_LL > down_LL) {
+    pushoff_LL = down_LL;
+  };
+
+  // Medial in stance
+  // LATERAL (outer):
+
+  servoRL.write(pushoff_RL);   //PUSH OFF
+  servoLL.write(pushoff_LL);  //PUSH OFF
+
+  //lateral_contactDelay.stopFromISR();
+  lateral_pushOff.changePeriodFromISR(pushOff);
+}
+
+void pushoff_lateral(){
+  servoRL.write(up_RL);     //RETRACT for swing
+  servoLL.write(up_LL);     //RETRACT for swing
+
+  //lateral_pushOff.stopFromISR();
+  lateral_swing.changePeriodFromISR(swing);
+
+}
+
+void swing_lateral(){               //SWING
+
+  servoRL.write(mid_RL);    //RESET
+  servoLL.write(mid_LL);    //RESET
+
+  switchState = !switchState;
+
+  //lateral_swing.stopFromISR();
+
+}
+
+
+//medials
+void contactDelay_medial(){
+
+  time2 = millis();
+  //     Serial.println(time2-time1);
+
+  pushoff_RM = mid_RM + (down_RM-mid_RM)*(time2-time1)/pushoffTimeSFM;
+  if (pushoff_RM > down_RM) {
+    pushoff_RM = down_RM;
+  };
+
+  pushoff_LM = mid_LM - (mid_LM-down_LM)*(time2-time1)/pushoffTimeSFM;
+  if (pushoff_LM < down_LM) {
+    pushoff_LM = down_LM;
+  };
+
+  /*   Serial.println(0);
+  Serial.println(pushoff_RM-mid_RM);
+  Serial.println(pushoff_LM-mid_LM);
+  Serial.println(0);
+  */
+
+  // Lateral in stance
+  // MEDIAL (inner):
+  servoRM.write(pushoff_RM);   //PUSH OFF
+  servoLM.write(pushoff_LM);   //PUSH OFF
+
+  //medial_contactDelay.stopFromISR();
+  medial_pushOff.changePeriodFromISR(pushOff);
+
+}
+
+void pushoff_medial(){
+  servoLM.write(up_LM);     //RETRACT for swing
+  servoRM.write(up_RM);     //RETRACT for swing
+
+  //medial_pushOff.stopFromISR();
+  medial_swing.changePeriodFromISR(swing);
+
+}
+
+void swing_medial(){               //SWING
+
+  servoRM.write(mid_RM);    //RESET
+  servoLM.write(mid_LM);    //RESET
+
+  switchState = !switchState;
+
+  //medial_swing.stopFromISR();
+
+}
+
+
 void setup() {
   pinMode(footSwitchM, INPUT);
   pinMode(footSwitchL, INPUT);
@@ -70,87 +183,100 @@ void setup() {
   servoLL.write(mid_LL);
   servoRM.write(mid_RM);
   servoLM.write(mid_LM);
+
+  lateral_contactDelay.stop();
+  lateral_pushOff.stop();
+  lateral_swing.stop();
+  medial_contactDelay.stop();
+  medial_pushOff.stop();
+  medial_swing.stop();
   delay(2000);
 
- //   Serial.begin(9600);
+  //   Serial.begin(9600);
 }
 
 void loop() {
 
-    int buttonStateM = digitalRead(footSwitchM);
-    int buttonStateL = digitalRead(footSwitchL);
+  int buttonStateM = digitalRead(footSwitchM);
+  int buttonStateL = digitalRead(footSwitchL);
 
-    if(buttonStateM == HIGH && switchState){ //if MEDIAL contact...
-      delay(contactDelay);
+  if(buttonStateM == HIGH && switchState){ //if MEDIAL contact...
+    lateral_contactDelay.changePeriod(contactDelay);
+    /* delayed version
+    delay(contactDelay);
 
-      time1 = millis();
-//     Serial.println(time1-time2);
+    time1 = millis();
+    //     Serial.println(time1-time2);
 
-        pushoff_RL = mid_RL - (mid_RL-down_RL)*(time1-time2)/pushoffTimeSFL; //450
-        if (pushoff_RL < down_RL) {
-        pushoff_RL = down_RL;
-        };
+    pushoff_RL = mid_RL - (mid_RL-down_RL)*(time1-time2)/pushoffTimeSFL; //450
+    if (pushoff_RL < down_RL) {
+    pushoff_RL = down_RL;
+  };
 
-        pushoff_LL = mid_LL + (down_LL-mid_LL)*(time1-time2)/pushoffTimeSFL;
-        if (pushoff_LL > down_LL) {
-        pushoff_LL = down_LL;
-        };
+  pushoff_LL = mid_LL + (down_LL-mid_LL)*(time1-time2)/pushoffTimeSFL;
+  if (pushoff_LL > down_LL) {
+  pushoff_LL = down_LL;
+};
 
-      // Medial in stance
-      // LATERAL (outer):
+// Medial in stance
+// LATERAL (outer):
 
-      servoRL.write(pushoff_RL);   //PUSH OFF
-      servoLL.write(pushoff_LL);  //PUSH OFF
-      delay(pushOff);
-      servoRL.write(up_RL);     //RETRACT for swing
-      servoLL.write(up_LL);     //RETRACT for swing
+servoRL.write(pushoff_RL);   //PUSH OFF
+servoLL.write(pushoff_LL);  //PUSH OFF
+delay(pushOff);
+servoRL.write(up_RL);     //RETRACT for swing
+servoLL.write(up_LL);     //RETRACT for swing
 
-      delay(swing);               //SWING
+delay(swing);               //SWING
 
-      servoRL.write(mid_RL);    //RESET
-      servoLL.write(mid_LL);    //RESET
+servoRL.write(mid_RL);    //RESET
+servoLL.write(mid_LL);    //RESET
 
-      switchState = !switchState;
+switchState = !switchState;
+*/
+}
+else if(buttonStateL == HIGH && !switchState){ //if LATERAL contact...
+  medial_contactDelay.changePeriod(contactDelay);
 
-    }
-    else if(buttonStateL == HIGH && !switchState){ //if LATERAL contact...
-      delay(contactDelay);
 
-      time2 = millis();
-//     Serial.println(time2-time1);
+  /*delayed version
+  delay(contactDelay);
 
-        pushoff_RM = mid_RM + (down_RM-mid_RM)*(time2-time1)/pushoffTimeSFM;
-        if (pushoff_RM > down_RM) {
-        pushoff_RM = down_RM;
-        };
+  time2 = millis();
+  //     Serial.println(time2-time1);
 
-        pushoff_LM = mid_LM - (mid_LM-down_LM)*(time2-time1)/pushoffTimeSFM;
-        if (pushoff_LM < down_LM) {
-        pushoff_LM = down_LM;
-        };
+  pushoff_RM = mid_RM + (down_RM-mid_RM)*(time2-time1)/pushoffTimeSFM;
+  if (pushoff_RM > down_RM) {
+  pushoff_RM = down_RM;
+};
 
-    /*   Serial.println(0);
-     Serial.println(pushoff_RM-mid_RM);
-     Serial.println(pushoff_LM-mid_LM);
-     Serial.println(0);
-  */
+pushoff_LM = mid_LM - (mid_LM-down_LM)*(time2-time1)/pushoffTimeSFM;
+if (pushoff_LM < down_LM) {
+pushoff_LM = down_LM;
+};
 
-      // Lateral in stance
-      // MEDIAL (inner):
-      servoRM.write(pushoff_RM);   //PUSH OFF
-      servoLM.write(pushoff_LM);   //PUSH OFF
-      delay(pushOff);
-      servoLM.write(up_LM);     //RETRACT for swing
-      servoRM.write(up_RM);     //RETRACT for swing
+//Serial.println(0);
+//Serial.println(pushoff_RM-mid_RM);
+//Serial.println(pushoff_LM-mid_LM);
+//Serial.println(0);
 
-      delay(swing);               //SWING
 
-      servoRM.write(mid_RM);    //RESET
-      servoLM.write(mid_LM);    //RESET
+// Lateral in stance
+// MEDIAL (inner):
+servoRM.write(pushoff_RM);   //PUSH OFF
+servoLM.write(pushoff_LM);   //PUSH OFF
+delay(pushOff);
+servoLM.write(up_LM);     //RETRACT for swing
+servoRM.write(up_RM);     //RETRACT for swing
 
-      switchState = !switchState;
+delay(swing);               //SWING
 
-    }
+servoRM.write(mid_RM);    //RESET
+servoLM.write(mid_LM);    //RESET
+
+switchState = !switchState;
+*/
+}
 
 
 
